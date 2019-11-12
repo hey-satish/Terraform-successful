7b7d91ffdd2eb8bc699323fa80c5922ee9a834c7
root <root@ip-172-31-28-161.us-east-2.compute.internal>
2019-11-12 05:51:19 +0000

    ec2cr commit

---
+++ ecr.tf
@@ -0,0 +1,5 @@
+resource "aws_ecr_repository" "myfirstapplication" {
+  name = "myfirstapplication"
+}
+
+
---
+++ output.tf
@@ -0,0 +1,4 @@
+output "myapp-repository-URL" {
+  value = aws_ecr_repository.myfirstapplication.repository_url
+}
+
